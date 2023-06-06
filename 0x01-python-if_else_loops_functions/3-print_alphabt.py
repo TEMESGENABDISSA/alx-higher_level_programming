e260559a395bbc78e3c8610ce62bc3d654b485f8
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-06-06 16:52:41 +0000

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Author - Temesgen Abdissa
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for c in range(ord('a'), ord('z')+1):
+    if c not in [ord('q'), ord('e')]:
+        print(chr(c), end='')
