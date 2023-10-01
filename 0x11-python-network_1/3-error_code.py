401f61379f5e95102fbec4c2a4d6c68b0ba41e80
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-01 01:16:32 -0700

    script that takes in a URL, sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
+usage:use the packages urllib and sys.
+  - Handles HTTP errors
 """
- Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
-"""
-from urllib import request, error
 import sys
-
+import urllib.error
+import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
-
     request = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(request) as response:
