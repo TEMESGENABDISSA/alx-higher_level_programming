9ce76f796a12aac2f575cd79d7f58c16d43a7d1f
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-01 00:54:02 -0700

    Task Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""A Displays the X-Request-Id header variable of a request to a given URL.
-Usage: ./1-hbtn_header.py <URL> or value of the X-Request-Id variable found in the header
-"""
+""" module document """
 import urllib.request
 import sys
 if __name__ == "__main__":
