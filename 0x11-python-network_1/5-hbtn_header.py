ae1721156c9768e4f124b0bf85643ee73ca0203a
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-01 01:28:26 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id"""
+"""a Python script that takes in a URL, sends a request to the URL &  displays"""
 import sys
 import requests
 if __name__ == "__main__":
