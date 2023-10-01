45aa512ee1cad857ec73b7ac9ddda8619a3e2839
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-01 01:50:32 -0700

    A script that sends a request to the URL &  displays the value of a variable in the response header

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""A  script that sends a request to the URL and
-   displays the value of a variable in the response header.
-"""
+"""A script that sends a request to the URL &  displays the value of a variable in the response header."""
 import sys
 import requests
-
-
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
