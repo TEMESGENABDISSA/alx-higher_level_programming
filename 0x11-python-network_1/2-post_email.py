589c4c0ec2ba45da2f0e395f09d537ecddcfe31e
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-01 01:10:12 -0700

    script that takes in a URL and an email, sends a POST request to the passed URL

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,13 @@
- #!/usr/bin/python3
-""" A Python script that takes in a URL and an email, sends a POST request to the passed URL """
+#!/usr/bin/python3
+""" A script Send a POST request to a given URL withinn the  email.
+"""
+import sys
 import urllib.parse
 import urllib.request
-import sys
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    params = {"email": email}
-    utf_params = urllib.parse.urlencode(params).encode('utf-8')
-    req = urllib.request.Request(url, data=utf_params, method='POST')
-    with urllib.request.urlopen(req) as res:
-        print(res.read().decode('utf-8'))
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
