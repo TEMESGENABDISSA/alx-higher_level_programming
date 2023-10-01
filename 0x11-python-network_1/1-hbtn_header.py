7076038c6e858cc612035cbbf469ae826c30a394
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-01 00:56:53 -0700

    Task Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" module document """
+""" A  script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id  
+  and  use the packages urllib and sys
+"""
 import urllib.request
 import sys
 if __name__ == "__main__":
