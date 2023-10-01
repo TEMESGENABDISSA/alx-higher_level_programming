e9cc26f6f1b558dbfdf6064de05fbacf38399af2
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-01 01:43:10 -0700

    script that takes your GitHub credentials (username and password) and uses the GitHub API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that takes your GitHub credentials (username and password) and uses the GitHub API """
+""" script that takes your GitHub credentials & uses the GitHub API """
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
