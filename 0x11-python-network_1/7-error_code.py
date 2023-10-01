5f11c5715282181785b2dc8112849c6482ea2335
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-10-01 02:07:30 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,16 +2,13 @@
 """
  A  script that sends a request to the URL and
 displays:
-- The body of the response if there are no errors,
-- The error code when there is an HTTP error.
+-- The body of the response if there are no errorr and 
+-- The error code when there is an HTTP error.
 """
 import sys
 import requests
-
-
 if __name__ == "__main__":
     url = sys.argv[1]
-
     r = requests.get(url)
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
