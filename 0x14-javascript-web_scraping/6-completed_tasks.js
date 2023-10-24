af1e253df4450fe7030201d3427b34bb495fc8e7
Temesgen Abdissa <temesegenabdissa2@gmail.com>
2023-10-24 12:21:08 -0700

    completed.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const url = process.argv[2];
-request(url, { json: true }, (err, res, body) => {
-  if (err) console.log(err);
-  const results = {};
-  for (const task of body) {
-    if (task.completed) {
-      results[task.userId] = (results[task.userId] || 0) + 1;
-    }
-  
+const out = {};
+function count (todo) {
+  if (todo.completed) {
+    const userId = todo.userId.toString();
+    out[userId] = out[userId] + 1 || 1;
   }
+}
 
-  console.log(results);
-
+request(url, (error, response, body) => {
+  if (error) throw error;
+  if (response.statusCode === 200) {
+    JSON.parse(body).forEach(count);
+    console.log(out);
+  }
 });
